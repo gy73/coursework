129eb55d8f9f912cb3c13f592c2582010142cb37
gy73 <ganievrazil006@gmail.com>
2025-03-31 13:00:03 +0300

    Add files via upload

---
+++ coursework-Front/coursework(Front)/src/components/Footer/Footer.jsx
@@ -0,0 +1,32 @@
+import './Footer.css'
+
+export default function Footer() {
+  return(
+    <>
+
+    <div className='lists' >
+      <ul className='lis1'>
+        <li> <a>Ссылка</a> </li>
+        <li> <a>Ссылка</a> </li>
+        <li> <a>Ссылка</a> </li>
+      </ul>
+      <ul className='lis2'>
+        <li> <a>Ссылка</a> </li>
+        <li> <a>Ссылка</a> </li>
+        <li> <a>Ссылка</a> </li>
+      </ul>
+      <ul className='lis3'>
+        <li> <a>Ссылка</a> </li>
+        <li> <a>Ссылка</a> </li>
+        <li> <a>Ссылка</a> </li>
+      </ul>
+    </div>
+      <div className='links'>
+        <a  href="/profile" className="icon-link"> <img src='/public/img/instagram-svgrepo-com.svg'/></a>
+        <a href="/profile" className="icon-link"> <img src='/public/img/telegram-svgrepo-com.svg'/></a>
+        <a href="/profile" className="icon-link"> <img src='/public/img/twitter-svgrepo-com.svg'/></a>
+
+      </div>
+    </>
+  )
+}
