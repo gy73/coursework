54884397d531f5c81b787cf14a3927a13f1ad9d4
gy73 <ganievrazil006@gmail.com>
2025-04-14 13:33:21 +0300

    Add files via upload

---
+++ coursework-Front/src/components/Main/Main.jsx
@@ -0,0 +1,14 @@
+import ProductsCards from '../../components/Main/Products/ProductCards';
+
+function Main() {
+  return (
+    <>
+    <main className='main'>
+      <div className='container'>
+        <ProductsCards/>
+      </div>
+    </main>
+    </>
+  );
+}
+export default Main;
---
+++ coursework-Front/src/components/Main/Products.js
@@ -0,0 +1,57 @@
+
+export const products = [
+    {   
+        id: 1,
+        designer: "Тимур Безбородников",
+        description:'джинсы',
+        image: "https://granhetl.com/static/itemsphoto/mini/1508_1_1730792200.jpeg",
+        price: 5000
+    },
+    {   
+        id: 2,
+        designer: "Тимур Безбородников",
+        description:'джинсы',
+        image: "https://granhetl.com/static/itemsphoto/mini/1508_1_1730792200.jpeg",
+        price: 5000
+    },
+        {
+        id: 3,
+        designer: "Тимур Безбородников",
+        description:'джинсы',
+        image: "https://granhetl.com/static/itemsphoto/mini/1508_1_1730792200.jpeg",
+        price: 5000
+    },
+    {   id: 4,
+        designer: "Тимур Безбородников",
+        description:'джинсы',
+        image: "https://granhetl.com/static/itemsphoto/mini/1508_1_1730792200.jpeg",
+        price: 5000
+    },
+    {   
+        id: 5,
+        designer: "Тимур Безбородников",
+        description:'джинсы',
+        image: "https://granhetl.com/static/itemsphoto/mini/1508_1_1730792200.jpeg",
+        price: 5000
+    },
+    {   id:6,
+        designer: "Тимур Безбородников",
+        description:'джинсы',
+        image: "https://granhetl.com/static/itemsphoto/mini/1508_1_1730792200.jpeg",
+        price: 5000
+    },
+    {   
+        id: 7,
+        designer: "Тимур Безбородников",
+        description:'джинсы',
+        image: "https://granhetl.com/static/itemsphoto/mini/1508_1_1730792200.jpeg",
+        price: 5000
+    },
+    {   
+        id: 8,
+        designer: "Тимур Безбородников",
+        description:'джинсы',
+        image: "https://granhetl.com/static/itemsphoto/mini/1508_1_1730792200.jpeg",
+        price: 5000
+    }
+]
